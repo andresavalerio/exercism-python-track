249e34b7fa6eceb7653dec8d4aa298e00262a426
Andresa <andresalvalerio@gmail.com>
2021-07-09 00:56:37 -0300

    ex: perfect numbers

---
+++ perfect-numbers/perfect_numbers.py
@@ -0,0 +1,10 @@
+def classify(number):
+    if number < 1:
+        raise ValueError("number not positive")
+    f = {1 if number > 1 else 0}
+    for n in range(2, int((number**(1/2))+1)):
+        if number % n == 0:
+            f.add(n)
+            f.add(int(number/n))
+    sum_f = sum(f)
+    return 'perfect' if sum_f == number else ('abundant' if sum_f > number else 'deficient') 
